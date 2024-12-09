a84a50b8e6ba052bdd635882de58f579743fa561
Bonstech <paulbornaah@gmail.com>
2024-12-09 15:34:27 +0000

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -205,18 +205,5 @@ document.getElementById('share-button').addEventListener('click', function() {
     }
 });
 
-function shareHeadline(headline, image) {
-  if (navigator.share) {
-      navigator.share({
-          title: headline,
-          text: headline,
-          url: window.location.href, // You can replace this with the specific URL of the news article
-      })
-      .then(() => console.log('Successfully shared'))
-      .catch((error) => console.error('Something went wrong sharing:', error));
-  } else {
-      // Fallback for browsers that do not support the Web Share API
-      alert('Sharing is not supported in this browser.');
-  }
-}
+
 
